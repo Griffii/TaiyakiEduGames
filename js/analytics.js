7eabb8075f16e38519b054ed105d04a58b27e1e5
Ty Griffith <ty.griffith@protonmail.com>
2025-08-03 17:31:59 +0900

    Removed links to old url Removed analyitcs tag temporarily

--- js/analytics.js
+++ js/analytics.js
@@ -1,28 +1,2 @@
-(function() {
-    const hostname = window.location.hostname;
-    let measurementId = "";
-  
-    if (hostname.includes("github.io")) {
-      // GitHub Pages
-      measurementId = "G-77VF8YQJDR"; 
-    } else if (hostname.includes("griffii.com")) {
-      // Hostinger-hosted site
-      measurementId = "G-X9DNJ3WNTY"; 
-    } else {
-      console.warn("Unknown host, no analytics loaded.");
-      return;
-    }
-  
-    const script = document.createElement('script');
-    script.async = true;
-    script.src = `https://www.googletagmanager.com/gtag/js?id=${measurementId}`;
-    document.head.appendChild(script);
-  
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){ dataLayer.push(arguments); }
-    window.gtag = gtag;
-  
-    gtag('js', new Date());
-    gtag('config', measurementId);
-  })();
+
   
