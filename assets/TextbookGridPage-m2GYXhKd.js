67b391d55a46d6414293ea722fedaa32aabdb3c6
Griffii <154314912+Griffii@users.noreply.github.com>
2025-12-09 04:19:31 +0000

    Build: (4c0409f) Added a rainbow border to the Mystery Machine wager menu and toast

--- assets/TextbookGridPage-m2GYXhKd.js
+++
@@ -1,1 +0,0 @@
-import{T as e}from"./TextbookList-DIp4hC56.js";import{i as a,a as o,d as s,k as t,b as c,_ as r}from"./index-BUUQalc1.js";const _={class:"flashcard-grid-page"},d={class:"center-area"},n=a({__name:"TextbookGridPage",setup(i){return(p,m)=>(c(),o("section",_,[s("div",d,[t(e)])]))}}),b=r(n,[["__scopeId","data-v-d9204b0b"]]);export{b as default};
